88e989902ccddb88fa18e2d489a12f66b09500d1
arthurlair <arthur.lair@etudiant.univ-rennes1.fr>
2023-12-11 10:52:10 +0100

    ajout implementation Date.java

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,68 @@
 package fr.istic.vv;
 
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 class Date implements Comparable<Date> {
+    private LocalDate date;
+    public Date(int day, int month, int year) {
+        this.date = LocalDate.of(year, month, day);
+    }
 
-    public Date(int day, int month, int year) { }
+    public static boolean isValidDate(int day, int month, int year) {
+        try {
+            LocalDate.of(year, month, day);
+            return true;
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true; // Année bissextile
+        }
+        return false;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public LocalDate getDateToLocalDate(){
+        return this.date;
+    }
 
-    public Date nextDate() { return null; }
+    public Date nextDate() {
+        LocalDate nextDay = this.date.plusDays(1);
+        return new Date(nextDay.getDayOfMonth(),nextDay.getMonthValue(),nextDay.getYear());
+    }
 
-    public Date previousDate() { return null; }
+    public Date previousDate() {
+        LocalDate beforeDay = this.date.minusDays(1);
 
-    public int compareTo(Date other) { return 0; }
+        return new Date(beforeDay.getDayOfMonth(),beforeDay.getMonthValue(),beforeDay.getYear());
+    }
+
+    public int compareTo(Date other) {
+        /*
+        positif : `date` is posterior to `other`
+        négatif : `date` is anterior to `other`
+        0 : `date` and `other` represent the same date.
+        null : throws a `NullPointerException`
+        */
+        if(other == null){
+            throw new NullPointerException("Date enter is null");
+        }
+
+        LocalDate otherLocalDate=other.getDateToLocalDate();
+
+        if(this.date.equals(otherLocalDate)){ //`date` and `other` represent the same date.
+            return 0;
+        }
+        if (this.date.isAfter(otherLocalDate)){ //`date` is posterior to `other`
+            return 1;
+        }else{ //`date` is anterior to `other`
+            return -1;
+        }
+    }
 
 }
